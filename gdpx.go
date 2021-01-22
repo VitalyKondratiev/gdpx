a36fa5e4a644e3ba2eb98e8fb6c62691c475e9d6
Vitaly Kondratiev <kondratiev.vitaliy2012@yandex.ru>
2021-01-22 15:13:17 +0500

    Add structures for configs

--- gdpx.go
+++ gdpx.go
@@ -1,20 +1,39 @@
 package main
 
-import "io/ioutil"
-import "bufio"
-import "os"
-import "fmt"
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	files, err := GetDockerComposeConfigs("/home/user/Documents/GitProjects");
+	files, err := GetDockerComposeConfigs("/home/user/Documents/GitProjects")
 	if err != nil {
 		fmt.Println(err)
 	}
 	for _, filePath := range files {
 		fmt.Println("\033[0;31m" + filePath + "\033[0m")
-		ReadEnvFile(filePath)
+		config, _ := ReadEnvFile(filePath)
+		fmt.Println(config.nginxDomain, config.nginxPort)
 	}
+}
+
+type EnvironmentConfig struct {
+	nginxDomain string
+	nginxPort   int
+}
+
+type ProjectConfig struct {
+	projectPath   string
+	configPath    string
+	defaultConfig EnvironmentConfig
+}
+
+type GlobalConfig struct {
+	projects ProjectConfig
 }
 
 func GetDockerComposeConfigs(root string) ([]string, error) {
@@ -24,7 +43,7 @@ func GetDockerComposeConfigs(root string) ([]string, error) {
 		return files, err
 	}
 	for _, dir := range rootDirInfo {
-		if (dir.IsDir()) {
+		if dir.IsDir() {
 			dirInfo, err := ioutil.ReadDir(root + "/" + dir.Name())
 			if err != nil {
 				return files, err
@@ -32,40 +51,41 @@ func GetDockerComposeConfigs(root string) ([]string, error) {
 			hasComposeFile := false
 			hasEnvironmentFile := false
 			for _, file := range dirInfo {
-				if (file.Name() == "docker-compose.yml") {
+				if file.Name() == "docker-compose.yml" {
 					hasComposeFile = true
 				}
-				if (file.Name() == ".env") {
+				if file.Name() == ".env" {
 					hasEnvironmentFile = true
 				}
 			}
-			if (hasComposeFile && hasEnvironmentFile) {
-				files = append(files, root + "/" + dir.Name() + "/.env")
+			if hasComposeFile && hasEnvironmentFile {
+				files = append(files, root+"/"+dir.Name()+"/.env")
 			}
 		}
 	}
 	return files, nil
 }
 
-func ReadEnvFile(filePath string) ([]string, error) {
-	var lines []string
+func ReadEnvFile(filePath string) (EnvironmentConfig, error) {
+	var config EnvironmentConfig
 	file, err := os.Open(filePath)
-    if err != nil {
-        return lines, err
-    }
-    defer file.Close()
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	var siteDomain string
 	var nginxPort string
-    for scanner.Scan() {
-		if (strings.HasPrefix(scanner.Text(), "SITE_DOMAIN=")) {
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "SITE_DOMAIN=") {
 			siteDomain = strings.Trim(strings.ReplaceAll(scanner.Text(), "SITE_DOMAIN=", ""), " ")
 		}
-		if (strings.HasPrefix(scanner.Text(), "NGINX_PORT=")) {
+		if strings.HasPrefix(scanner.Text(), "NGINX_PORT=") {
 			nginxPort = strings.Trim(strings.ReplaceAll(scanner.Text(), "NGINX_PORT=", ""), " ")
 		}
 	}
-	fmt.Println(siteDomain + ":" + nginxPort)
-	return lines, nil
+	nginxPortInt, err := strconv.Atoi(nginxPort)
+	config = EnvironmentConfig{siteDomain, nginxPortInt}
+	return config, nil
 }
